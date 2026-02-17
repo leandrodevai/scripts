0c8aabb1deba9298aedb55f1b5d3db4ce69d4be6
Leandro Patrón <143761947+leandrodevai@users.noreply.github.com>
2026-02-17 20:23:56 -0300

    Update package installation in hf_dep.sh
    Added 'unsloth' and 'vllm flash-attn' to the package installation commands.

--- hf_dep.sh
+++ hf_dep.sh
@@ -9,7 +9,7 @@ set -eo pipefail
 . /venv/main/bin/activate
 
 # Install the required packages
-pip install transformers[sentencepiece] datasets evaluate wandb trl peft trackio huggingface_hub bitsandbytes accelerate
+pip install transformers[sentencepiece] datasets evaluate wandb trl peft trackio huggingface_hub bitsandbytes accelerate unsloth
 
 # Install support packages
-pip install ipykernel ipywidgets scikit-learn rouge-score bert-score pandas numpy lighteval pytest pytest-cov tqdm
+pip install ipykernel ipywidgets scikit-learn rouge-score bert-score pandas numpy lighteval pytest pytest-cov tqdm vllm flash-attn
